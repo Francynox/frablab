f9e65a54fa92b33a091a0fb12e37bfd46a49f6ab
Francesco Benini <francy.benini@gmail.com>
2026-05-09 00:30:26 +0200

    feat: complete bind configuration

--- hosts/mimir/bind.nix
+++ hosts/mimir/bind.nix
@@ -1,24 +1,101 @@
-{ inputs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 let
-  bindConfig = inputs.frablab-config + "/bind/named.conf";
+  cfg = config.services.francynox.bind;
+  dnsZones = {
+    staticZoneFiles = builtins.mapAttrs (
+      name: content: pkgs.writeText name content
+    ) inputs.frablab-config.lib.dnsZones.staticZoneFiles;
+    dynamicZoneFiles = builtins.mapAttrs (
+      name: content: pkgs.writeText name content
+    ) inputs.frablab-config.lib.dnsZones.dynamicZoneFiles;
+  };
+  bind = {
+    namedConf = pkgs.writeText "named.conf" (
+      inputs.frablab-config.lib.bind.namedConf {
+        inherit (cfg) dataDir;
+        dhcpKeyFile = "${dhcpKeyFile}";
+      }
+    );
+    inherit (inputs.frablab-config.lib.bind) rndcKey dhcpKey;
+  };
+
   bindConfigEtcPath = "bind/named.conf";
+  staticZonesPrefix = "bind/zones_static";
+  dhcpKeyFile = "/etc/bind/dhcp-ddns.key";
 in
 {
+  sops.secrets = {
+    bind-rndc-key = {
+      sopsFile = bind.rndcKey;
+      path = cfg.rndcKeyFile;
+      format = "json";
+      key = "data";
+      mode = "0600";
+      owner = cfg.user;
+      inherit (cfg) group;
+      restartUnits = [ "named.service" ];
+    };
+    bind-dhcp-key = {
+      sopsFile = bind.dhcpKey;
+      path = dhcpKeyFile;
+      format = "json";
+      key = "data";
+      mode = "0600";
+      owner = cfg.user;
+      inherit (cfg) group;
+      restartUnits = [ "named.service" ];
+    };
+  };
+
   services = {
     francynox = {
       bind = {
         enable = true;
         configFile = "/etc/${bindConfigEtcPath}";
+        staticZoneFiles = builtins.mapAttrs (
+          name: _: "/etc/${staticZonesPrefix}/${name}"
+        ) dnsZones.staticZoneFiles;
+        inherit (dnsZones) dynamicZoneFiles;
         extraRestartTriggers = [
-          bindConfig
-        ];
+          bind.namedConf
+        ]
+        ++ (lib.attrValues dnsZones.staticZoneFiles);
       };
 
       mutable-configs = {
         "${bindConfigEtcPath}" = {
-          source = bindConfig;
+          source = bind.namedConf;
         };
-      };
+      }
+      // (lib.mapAttrs' (
+        name: source:
+        lib.nameValuePair "${staticZonesPrefix}/${name}" {
+          inherit source;
+          pathToCheck = "${cfg.dataDir}/${name}";
+        }
+      ) dnsZones.staticZoneFiles);
+    };
+  };
+
+  frablab.base.persistence.additionalDirectories = [
+    "${cfg.dataDir}"
+  ];
+
+  networking = {
+    firewall = {
+      allowedTCPPorts = [
+        53
+        443
+      ];
+      allowedUDPPorts = [
+        53
+      ];
     };
   };
 }
